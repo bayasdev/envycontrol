008a604c420312813cb0751f495b5720c22ed356
ITesserakt <portyas85@mail.ru>
2024-02-25 04:40:51 +0300

    Simpified flake.nix to support only Linux x86_64

--- flake.nix
+++ flake.nix
@@ -3,32 +3,26 @@
 
   outputs = { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
-      packages = forAllSystems (system:
-        with pkgs.${system}.python3Packages; {
-          default = buildPythonPackage rec {
-            name = "envycontrol";
-            version = "3.3.1";
-            src = self;
-          };
-      });
-
-      buildInputs = forAllSystems (system:
-        with pkgs.${system}; [ pciutils ]
-      );
-
-      devShells = forAllSystems (system: let
-      in {
-        default = pkgs.${system}.mkShellNoCC {
-          packages = with pkgs.${system}; [
-            (python3.withPackages(ps: with ps; [ setuptools ]))
-            pciutils
-          ];
+      packages.${system} = {
+        envycontrol = pkgs.python3Packages.buildPythonPackage {
+          pname = "envycontrol";
+          version = "3.3.1";
+          src = self;
         };
-      });
+        default = self.packages.${system}.envycontrol;
+      };
+
+      buildInputs = with pkgs; [ pciutils ];
+
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          (python3.withPackages(ps: with ps; [ setuptools ]))
+          pciutils
+        ];
+      };
     };
 }
